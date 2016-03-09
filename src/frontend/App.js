cf5d5905b2780d5962d55410b5f5af018fcd50ee
Praneeta Jhanwar <praneetaj@zillowgroup.com>
2016-03-09 15:37:42 -0800

    Added AskToCall component

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import DatePicker from './components/smart/datePicker';
-import InterestedInTouring from './components/dumb/InterestedInTouring';
-import ChatBubble from './components/smart/ChatBubble';
+import DatePicker from './components/DatePicker';
+import AskToCall from './components/AskToCall';
+// import ChatBubble from './components/ChatBubble';
 
 import 'normalize.css'
 import './scss/main.scss';
@@ -10,8 +10,8 @@ export default class App extends Component {
         render() {
                 return (
                     <div className='App__container'>
-                            <ChatBubble />
                             <DatePicker />
+                            <AskToCall />
                             <div className="App__body">
                                     {this.props.children}
                             </div>
--- src/frontend/components/AskToCall/index.js
+++ src/frontend/components/AskToCall/index.js
@@ -3,13 +3,42 @@ import React, {Component} from 'react';
 import './AskToCall.scss';
 
 export default class AskToCall extends Component {
-  constructor(props) {
-    super(props);
+  render() {
+    return  (
+      <div className='AskToCall'>
+        <div>
+          Excellent. We&#39;d love to give you a call to finalize some details.
+          <br/>
+          <br/>
+          Would that be okay?
+          <hr/>
+        </div>
+        <div>
+          <div>
+            <div>
+              <a  onClick={this.handleYesClick.bind(this)} className='StyleAnchorTags'>Yes</a>
+            </div>
+            <div>
+              <a  onClick={this.handleNoClick.bind(this)} className='StyleAnchorTags'>No</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   componentDidMount() {
+    this.setState({'requestToCall' : false});
   }
 
-  render() {
+  handleYesClick (){
+    this.setState({'requestToCall' : true});
+    // console.log(this.state);
   }
+
+  handleNoClick (){
+    this.setState({'requestToCall' : false});
+    // console.log(this.state);
+  }
+
 }
