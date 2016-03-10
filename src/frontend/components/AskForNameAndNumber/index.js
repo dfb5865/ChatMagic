86f7b8aeb0f414b1667fccbe42100d8959d94f9c
Praneeta Jhanwar <praneetaj@zillowgroup.com>
2016-03-09 20:25:26 -0800

    Added AskForNameAndNumber component

--- src/frontend/components/AskForNameAndNumber/index.js
+++ src/frontend/components/AskForNameAndNumber/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Label from '../Label';
 
 import './AskForNameAndNumber.scss';
 
@@ -11,5 +12,25 @@ export default class AskForNameAndNumber extends Component {
   }
 
   render() {
+    return (
+      <div className='ask-for-name-and-number'>
+        <div className='bubble-text div-with-bottom-border'>
+          Great! Tell us your name and phone number and we&#39;ll connect you with the best Premier Agent available to show the home.
+        </div>
+        <div className='div-with-bottom-border padded-div'>
+          <Label name='Name'/>
+          <input id='input-box-name' className='input-box-style'/>
+        </div>
+        <div className='div-with-bottom-border padded-div'>
+          <Label name='Phone Number'/>
+          <input className='input-box-style'/>
+        </div>
+        <div className='ok-text' onClick={this.handleOkClick.bind(this)}>OK</div>
+      </div>
+    );
+  }
+
+  handleOkClick() {
+    console.log('Hello');
   }
 }
