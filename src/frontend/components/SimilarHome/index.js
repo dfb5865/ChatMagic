868778422af19820a936307e2cd1f317e288d1ff
Dave Banan <daveba@zillow.com>
2016-03-10 14:36:42 -0800

    fix similar home appearing

--- src/frontend/components/SimilarHome/index.js
+++ src/frontend/components/SimilarHome/index.js
@@ -9,45 +9,21 @@ export default class SimilarHome extends Component {
 
     constructor(props) {
         super(props);
-        this.state = initialState;
     }
 
     render() {
       return (
         <div className='ask-for-name-and-number'>
           <div className='bubble-text div-with-bottom-border'>
-            Great! Tell us your name and phone number and we&#39;ll connect you with the best Premier Agent available to show the home.
+            Here's a similar home while you wait.
           </div>
           <div className='div-with-bottom-border padded-div'>
-            <input className='text-input' type="text" value={this.state.name} onChange={this.handleNameChange.bind(this)} onClick={this.clearName.bind(this)}/>
+            HOME
           </div>
           <div className='div-with-bottom-border padded-div'>
-            <input className='text-input' type="text" value={this.state.number} onChange={this.handleNumberChange.bind(this)} onClick={this.clearNumber.bind(this)}/>
+           HOME INFO
           </div>
-          <div className='ok-text' onClick={this.handleOkClick.bind(this)}>OK</div>
         </div>
       );
-    }
-
-    handleOkClick() {
-      var name = this.state.name;
-      var number = this.state.number;
-
-      axios.post('/api/nameAndNumber/', {name: name, number: number})
-      .then((response) => {
-          if(!response.data.questionResponse){
-              console.log("Failed to load response data");
-          }
-          else{
-              var responseText = response.data.questionResponse;
-              var nextQuestionBubbleType = response.data.nextQuestionBubbleType;
-
-              this.props.addPanel(<ResponseBubble addPanel={this.props.addPanel} message={responseText} />);
-              this.props.addPanel(<QuestionBubble addPanel={this.props.addPanel} type={nextQuestionBubbleType} />);
-          }
-      })
-      .catch(function (response, err) {
-          console.log(err);
-      });
     }
 }
