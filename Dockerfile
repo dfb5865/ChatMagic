4be9dc2c6ec04a7949369cb62579c93d65aa8de6
Dave Banan <daveba@zillow.com>
2016-03-14 07:09:56 -0700

    i was defeated trying to deploy to aws

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,3 @@
-FROM ubuntu:14.04
+FROM node:4-onbuild
 
-RUN apt-get update && apt-get upgrade -y
-
-RUN apt-get install -y git git-core wget zip nodejs npm
-
-EXPOSE 8080
-
-# startup
-ADD start.sh /tmp/  
-RUN chmod +x /tmp/start.sh  
-CMD ./tmp/start.sh  
+EXPOSE 4000
--- src/server/server.js
+++ src/server/server.js
@@ -7,14 +7,16 @@ var koaBody = require('koa-bodyparser');
 var mount = require('koa-mount');
 var api = require('./controllers/api');
 var ENV = process.env.NODE_ENV;
-var PORT = 8080;
+var PORT = 4000;
 var app = koa();
 
 app.use(logger());
 app.use(koaBody());
 
+app.use(mount('/', serve(path.join(__dirname, "views/"))));
 app.use(mount('/assets', serve(path.join(__dirname, "public/assets"))));
 app.use(mount('/api', api.routes()));
+
 
 app.listen(PORT, function () {
   console.log("HTTP server listening on", PORT);
